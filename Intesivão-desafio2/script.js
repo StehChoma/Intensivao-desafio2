7aeab765d4ae37709ffc40f72dcf59224982ea5f
StehChoma <105441843+StehChoma@users.noreply.github.com>
2023-09-06 16:52:51 -0300

    Add files via upload

---
+++ Intesivão-desafio2/script.js
@@ -0,0 +1,78 @@
+/* 1
+alert ("Hello World")*/
+
+/* 2
+const numA = 15
+const numB = 37
+
+let sum = numA + numB
+alert(sum) */
+
+
+/*3
+const coisa = 30
+
+  if (typeof coisa == 'number') {
+    console.log ("É um número")
+} else {
+  console.log("Não é um número")
+}
+
+
+
+/*4
+const coisaOne = "happy"
+
+if (typeof coisaOne == 'string') {
+  console.log ("É uma string")
+} else {
+  console.log ("Não é uma string")
+} */
+
+/*5
+const qualTipo = true
+
+if (typeof qualTipo == 'boolean') {
+  console.log ("É um booleano",qualTipo)
+} else {
+  console.log ("Não é um booleano")
+}
+*/
+
+/*6
+const numberOne = 57
+const numberTwo = 13
+const sub = numberOne - numberTwo
+console.log (sub) */
+
+/*7
+const numberOne = 7
+const numberTwo = 5
+const multi = numberOne * numberTwo
+console.log(multi) */
+
+/*8
+const numberOne = 35
+const numberTwo = 7
+const div = numberOne / numberTwo
+console.log(div) */
+
+/*9
+const number = 91
+let resto = number % 2
+if(resto == 0){
+  console.log("É um número par")
+} else {
+  console.log("Não é um número par")
+}*/
+
+/*10
+const number = 27
+const resto = number %2
+
+if(resto != 0){ 
+  console.log("É um número impar")
+} else {
+  console.log("Não é um número impar")
+}
+*/
